02f1f051224cb82992b03c9451c5f89c58833e65
CeliaPe <celia.pedregosa@itgranada.com>
2022-01-25 10:44:27 +0100

    Vectores

--- dia3.cpp
+++ dia3.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
+/* VOLVEMOS MAÑANA
+void mostrarArray (string array[]){
+  for(int i = 0 ; i < array.size() ; i++){
+    cout << i << " = " << array[i] << endl;
+  }
+}*/
+
 int main(){
+  int numJuegos = 10;
+  string juegos[numJuegos];
+
+  for(int i = 0 ; i < numJuegos ; i++){
+    string jue;
+    cout << "Introduce el nombre de un juego: ";
+    cin >> jue;
+    juegos[i] = jue;
+  }
+
+  juegos[4] = "Tetris";
+
+  for(int i = 0 ; i < numJuegos ; i++){
+    cout << "Juegos" << i << " = " << juegos[i] << endl;
+  }
+
+
+
+
+
+  //mostrarArray(juegos);
 
   return 0;
 }
