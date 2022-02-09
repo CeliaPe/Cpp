fc8c3c6cc6eda893a4240028f447bd1552a10abc
CeliaPe <celia.pedregosa@itgranada.com>
2022-02-09 12:33:09 +0100

    Concepto puntero

--- dia4.cpp
+++ dia4.cpp
@@ -6,9 +6,13 @@ using namespace std;
 int main() {
   string juego = "Tetris";
   string &ocio = juego;
-  string juegos[5];
+  int num = 5;
+  //string juegos[5];
+  string* ptrJuego = &juego;
+  int* ptrNum = &num;
 
-  cout << juego << endl; // Imprime Tetris.
+
+  /*cout << juego << endl; // Imprime Tetris.
   cout << ocio << endl; // Imprime Tetris.
   cout << &ocio << endl; // Imprime la dirección.
   cout << &juego << endl; // Imprime la dirección.
@@ -24,6 +28,23 @@ int main() {
   cout << &juegos[2] << endl;
   cout << &juegos[3] << endl;
   cout << &juegos[4] << endl;
+  */
+
+  cout << "Imprimo el juego: " << juego << endl;
+  cout << "Imprimo el &juego: " << &juego << endl;
+  //cout << "Imprimo el *juego: " << *juego << endl;
+  cout << "Imprimo el ocio: " << ocio << endl;
+  cout << "Imprimo el &ocio: " << &ocio << endl << endl;
+  cout << "Imprimo el ptrJuego: " << ptrJuego << endl;
+  cout << "Imprimo el &ptrJuego: " << &ptrJuego << endl;
+  cout << "Imprimo el *ptrJuego: " << *ptrJuego << endl << endl;
+  cout << "Imprimo el num: " << num << endl;
+  cout << "Imprimo el *ptrNum: " << *ptrNum << endl;
+  cout << "Imprimo el &num: " << &num << endl;
+  cout << "Imprimo el ptrNum: " << ptrNum << endl << endl;
+
+
+
 
 
 
