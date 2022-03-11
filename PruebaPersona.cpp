7c063bf87f60abe973b39b50be1191a12705761b
CeliaPe <celia.pedregosa@itgranada.com>
2022-03-11 09:04:08 +0100

    Lo básico

---
+++ PruebaPersona.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Persona{
+  private:
+    string nombre;
+    string dni;
+    int edad;
+  public:
+    Persona(string nombre, string dni, int edad = 18){
+      this->nombre = nombre;
+      this->dni = dni;
+      this->edad = edad;
+    }
+    string getNombre(){
+      return this->nombre;
+    }
+    string getDni(){
+      return this->dni;
+    }
+    int getEdad(){
+      return this->edad;
+    }
+    void setNombre(string nombre){
+      this->nombre = nombre;
+    }
+    void setDni(string dni){
+      this->dni = dni;
+    }
+    void setEdad(int edad){
+      this->edad = edad;
+    }
+    bool esMayorEdad(){
+      return this->edad>=18;
+    }
+    void imprimir(){
+      cout << "Me llamo " << this->nombre << ", tengo " << this->edad << " años y mi DNI es " << this->dni << endl;
+      if (this->esMayorEdad()) cout << "Es mayor de edad." << endl;
+    }
+};
+
+int main(){
+  Persona miPersona("Celia","12345678H",26);
+  Persona otraPersona("Isaac","87654321H");
+
+  cout << "Comprobando que imprimir() funciona : " << endl;
+  miPersona.imprimir();
+  cout << "Comprobando que el constructor por defecto fuciona: " << endl;
+  otraPersona.imprimir();
+  cout << "Comprobando que los get funcionan: " << endl;
+  cout << miPersona.getNombre() << " " << miPersona.getDni() << " " << miPersona.getEdad() << endl;
+  cout << "Comprobando que los set funcionan: " << endl;
+  miPersona.setNombre("Lucas");
+  miPersona.setEdad(15);
+  miPersona.setDni("23456781A");
+  miPersona.imprimir();
+
+  return 0;
+}
