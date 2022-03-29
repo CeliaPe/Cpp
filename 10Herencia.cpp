f82707c667b7d84b3528f10252856f02d1f99bfc
CeliaPe <celia.pedregosa@itgranada.com>
2022-03-29 08:58:09 +0200

    Diferencia entre herencia public y private

--- 10Herencia.cpp
+++ 10Herencia.cpp
@@ -43,14 +43,20 @@ Cuando definimos la herencia de una clase:
       private   : No podemos acceder.
       protected : Protegido.
       public    : Publico.
+        Esto quiere decir que la clase que hereda deja las funciones tal y como
+        las tenía la clase padre.
   Protected:
       private   : No podemos acceder.
       protected : Protegido.
       public    : Protegido.
+        Esto quiere decir que la clase que hereda tranforma las funciones que
+        la clase padre tiene públicas en privadas.
   Privada:
       private   : No podemos acceder.
       protected : Privado.
       public    : Privado.
+        Esto quiere decir que la clase que hereda tranforma las funciones que
+        la clase padre tiene como protegidas o públicas en privadas.
 */
 class Alumno : public Persona{
   private:
@@ -83,17 +89,56 @@ class Alumno : public Persona{
       cout << "Estoy en el grado de " << this->grado << " y estoy en el curso "
            << this->curso << "." << endl;
     }
+
+    void probandoHerencia(){
+      cout << "Si estoy en una herencia public: " << endl;
+      //cout << "Llamo a algo privado: " << this->dni << endl;
+      cout << "Llamo a algo publico: " << this->getDni() << endl;
+    }
+};
+
+class Profesor : private Persona{
+  private:
+    double sueldo;
+  public:
+    Profesor (string nombre = "nombreProf", string dni = "dniProf",
+              double sueldo = 2000.00 ) : Persona (nombre,dni){
+      this->sueldo = sueldo;
+    }
+    void setSueldo(double sueldo){
+      this->sueldo = sueldo;
+    }
+    double getSueldo(){
+      return this->sueldo;
+    }
+
+    void imprimir(){
+      // No podemos llamar al imprimir del padre, porque estamos especializando
+      // la función ahora: this->imprimir();
+      Persona::imprimir();
+      // cout << "Me llamo " << this->getNombre() << " y mi DNI es " <<
+      // this->getDni() << "." << endl;
+      cout << "Mi sueldo es de " << this->sueldo << "euros." << endl;
+    }
+    void probandoHerencia(){
+      cout << "Si estoy en una herencia public: " << endl;
+      //cout << "Llamo a algo privado: " << this->dni << endl;
+      cout << "Llamo a algo publico: " << this->getDni() << endl;
+    }
 };
 
 int main(){
-  //Persona miPersona;
   Alumno alumnoDef;
   Alumno miAlumno("miNombre","miDni","miGrado",2);
   Alumno* ptrAlumno;
-  //miPersona.imprimir();
+  Profesor profDef;
   ptrAlumno = new Alumno("ptrNombre","ptrDni","ptrGrado",9);
   alumnoDef.imprimir();
   miAlumno.imprimir();
   ptrAlumno->imprimir();
+  profDef.imprimir();
+  cout << alumnoDef.getDni();
+  // cout << profDef.getDni();  // Da error porque la herencia a tranformado una
+                                // función que era pública a privada...*/
   return 0;
 }
